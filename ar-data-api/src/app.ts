ae6ea102bdd04ba36f8132b7fffe17c3af96ef1a
Aubrey Malabie <malengatiger@gmail.com>
2019-05-27 00:01:01 +0200

    🌽 first successful MongoDB write - 💙  💚  💛

--- ar-data-api/src/app.ts
+++ ar-data-api/src/app.ts
@@ -8,67 +8,44 @@ const password = process.env.MONGODB_PASSWORD || "aubrey3";
 const user = process.env.MONGODB_USER || "aubs";
 const appName = "AR MongoDB API";
 const mongoConnection = `mongodb+srv://${user}:${password}@ar001-1xhdt.mongodb.net/test?retryWrites=true`;
-import AssociationController from "./routes/associations";
+import {Routes} from "./routes/app_routes";
 
 mongoose.connect(
     mongoConnection, {
         useNewUrlParser: true,
     });
 console.log(`\n\Mongoose connected ... 🔆 🔆 🔆 🔆 🔆`);
-function loggerMiddleware(request: express.Request, response: express.Response, next) {
-    console.log(`🔆 🔆 request method: 🍎  ${request.method} path: 🍎  ${request.path}`);
-    next();
-  }
-// app.use(loggerMiddleware);
-// app.use(morgan("dev"));
-// app.use(bodyParser.urlencoded({ extended: false}));
-// app.use(bodyParser.json());
 
-// app.get("/", (req, res) => {
-//   res.send("The sedulous hyena ate the antelope!");
-// });
-
-// app.listen(port, () => {
-//     console.log(`♻️ ♻️ ♻️ mongoDB connection:  ♻️  ${mongoConnection}  ♻️`);
-//     return console.log(`🏮 🏮 🏮  Server is listening on ${port} for app: 💚 💙 💜  ${appName} :  🍎  ${new Date()}`);
-// });
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" );
-// });
-
-// app.use("/associations", AssociationController.call("");
+// function loggerMiddleware(request: express.Request, response: express.Response, next) {
+//     console.log(`🔆 🔆 request method: 🍎  ${request.method} path: 🍎  ${request.path}`);
+//     next();
+//   }
 
 class App {
     public app: express.Application;
     public port: number;
+    public routes: Routes = new Routes();
 
-    constructor(controllers: any, port: number) {
+    constructor() {
         console.log(`\n\n🦀 🦀 🦀 🦀 🦀    ---   Inside App constructor `);
         this.app = express();
-        this.port = port;
+        this.port = 5000;
         this.initializeMiddlewares();
-        this.initializeControllers(controllers);
+        this.routes.routes(this.app);
     }
 
     public listen() {
       this.app.listen(this.port, () => {
-        console.log(`\n\n🥦 🥦 🥦 🥦   ---   App listening on port 🥦  💦  ${this.port}  💦`);
+        console.log(`\n\n🥦 🥦 🥦 🥦   ---   AR MongoDB API listening on port 🥦  💦  ${this.port}  💦`);
       });
     }
 
     private initializeMiddlewares() {
-        console.log(`\n🥦 🥦  initializeMiddlewares`);
+        console.log(`\n🥦 🥦  initializeMiddleware .... `);
         this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
     }
 
-    private initializeControllers(controllers) {
-        console.log(`\n🥦 🥦  initializeControllers`);
-        controllers.forEach((controller: AssociationController) => {
-        console.log(controller);
-        this.app.use(controller.path, controller.router);
-      });
-    }
   }
 
 export default App;
--- ar-data-api/src/server.ts
+++ ar-data-api/src/server.ts
@@ -1,9 +1,8 @@
 import App from "./app";
-import AssociationController from "./routes/associations";
 
 console.log(
   `\n\n server.ts: 🌽 🌽 🌽   Constructing new App object .... 🌽 🌽 🌽 `
 );
-const app = new App([new AssociationController()], 5000);
+const app = new App();
 
 app.listen();
